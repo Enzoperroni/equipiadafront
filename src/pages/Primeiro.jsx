ff7774c058968f3a75130fba0302ac99c25f09df
Enzo <202201037598@alunos.ibmec.edu.br>
2023-10-11 16:47:16 -0300

    configurando bibliotecas

--- src/pages/Primeiro.jsx
+++ src/pages/Primeiro.jsx
@@ -1,7 +1,10 @@
+import LinkSegundo from "../components/LinkSegundo";
+
 const Primeiro = ()=> {
  return (<div>
     <h1>Mini titulo</h1>
     <p>Meu texto</p>
+    <LinkSegundo/>
     </div>);
 };
 
