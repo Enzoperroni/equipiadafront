eb609cbcf091565e668b0802f3378979e3c9cb91
Enzo <202201037598@alunos.ibmec.edu.br>
2023-10-18 16:43:31 -0300

    something

--- src/pages/Primeiro.jsx
+++ src/pages/Primeiro.jsx
@@ -1,10 +1,29 @@
+import LinkGenerico from "../components/LinkGenérico";
 import LinkSegundo from "../components/LinkSegundo";
 
 const Primeiro = ()=> {
+    const dados = [
+        [ "https://www.ibmec.br/", "ibmec" ],
+        [ "https://estude.ibmec.br/ibmec-hubs","ibmec-hubs"],
+        [ "https://botafogo.com.br/", "botafogo"],
+        [ "https://github.com/", "github"],
+        [ "https://foundation.mozilla.org/pt-BR/", "mozilla"],
+
+    ];
+
+    const links = dados.map( (ele,i) => 
+    (<LinkGenerico
+        key = {i}
+        texto = {(ele[1])}
+        endereco = {(ele[0])} 
+        />
+    ));
+
+    
  return (<div>
     <h1>Mini titulo</h1>
     <p>Meu texto</p>
-    <LinkSegundo/>
+    {links}
     </div>);
 };
 
