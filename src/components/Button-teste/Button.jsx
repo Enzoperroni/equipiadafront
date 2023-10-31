bbd448f2b4af3d5bf50635c9ce69f35bb2d38164
Joao Pedro Passos Ribeiro de Menezes <86020181+Mnzss1133@users.noreply.github.com>
2023-10-31 17:55:04 -0300

    Update Button.jsx

--- src/components/Button-teste/Button.jsx
+++ src/components/Button-teste/Button.jsx
@@ -1,16 +1,1 @@
-import React from 'react';
 
-import { EstilodoBotao } from './style';
-
-
-
-function MyButton() {
-  return (
-    
-      <EstilodoBotao></EstilodoBotao>  
-     
-    
-  );
-}
-
-export default MyButton;
