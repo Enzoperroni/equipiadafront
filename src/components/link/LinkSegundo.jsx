0495a34a762ae35c09a619996a7c1363b7035b72
Enzo Perroni <zocaperroni@gmail.com>
2023-11-22 16:54:33 -0300

    link dnv

--- src/components/link/LinkSegundo.jsx
+++ src/components/link/LinkSegundo.jsx
@@ -2,7 +2,7 @@ import { ImageSegundoLinkContainer,LinkSegundoImage } from "./StyleSegundo"
 
 const SegundoLink = () => {
     return (
-      <ImageSegundoLinkContainer href="/equipiadafront">
+      <ImageSegundoLinkContainer href="equipiada">
         <LinkSegundoImage src="https://i.pinimg.com/originals/37/8d/ca/378dcab29f599b5508ec838583195097.png" alt="Image Description" />
         
       </ImageSegundoLinkContainer>
