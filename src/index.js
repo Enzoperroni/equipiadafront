56584b54c5c00f2febbc11a7f323c624d4711333
Enzo Perroni <zocaperroni@gmail.com>
2023-11-22 00:06:25 -0300

    fixtryout

--- src/index.js
+++ src/index.js
@@ -11,10 +11,10 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <HashRouter>
-    <Routes>
-      <Route path="/" element={<Primeiro />} />
-      <Route path="/segundo" element={<Segundo/>} />
-    </Routes>
+      <Routes>
+        <Route path="/" element={<Primeiro />} />
+        <Route path="#/segundo" element={<Segundo/>} />
+      </Routes>
     </HashRouter>
   </React.StrictMode>
 );
